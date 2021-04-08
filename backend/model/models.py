45327aa716b5615a07049141f1d3932cbb88cd44
suram <swamys@naver.com>
2021-04-08 17:36:37 +0900

    기능 : scene, line 모델 추가

--- backend/model/models.py
+++ backend/model/models.py
@@ -6,7 +6,8 @@ class Place(db.Model):
     movieid = db.Column(db.Integer, db.ForeignKey("movie.id"))
     name = db.Column(db.String(50))
     frequency = db.Column(db.Integer)
-    scene = db.Column(db.ARRAY(db.Integer))
+    scene = db.Column(db.PickleType)
+    array = db.Column(db.JSON)
 
 class Summary(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -20,3 +21,14 @@ class Character(db.Model):
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(200))
+
+class Scene(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    num = db.Column(db.Integer, unique=True)
+    movieid = db.Column(db.Integer, db.ForeignKey("movie.id"))
+
+class Line(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    scenenum = db.Column(db.Integer, db.ForeignKey("scene.num"))
+    content = db.Column(db.Text)
+    order = db.Column(db.SMALLINT)
