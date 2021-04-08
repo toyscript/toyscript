f70d2b685ead20c47144068824f2f03afe099acc
suram <swamys@naver.com>
2021-04-08 19:45:13 +0900

    기능 : 영화 제목 검색 api

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,12 +1,13 @@
 from flask_restful import Api
 from flask import Blueprint
 from resource.place import PlaceFrequency, PlaceScene
+from resource.query_movie import QueryMovie
 
 toyScriptApi = Blueprint("api", __name__)
 api = Api(toyScriptApi)
 
 api.add_resource(PlaceFrequency, '/place/frequency')
 api.add_resource(PlaceScene, '/place/scene')
-
+api.add_resource(QueryMovie, '/movie')
 
 
