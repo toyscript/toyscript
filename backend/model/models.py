5d6c8cca8970b702f1b1d1ce86c421ae968535e8
suram <swamys@naver.com>
2021-04-09 14:55:02 +0900

    기능 : DB 모델 수정

--- backend/model/models.py
+++ backend/model/models.py
@@ -2,22 +2,15 @@ from db.init_db import db
 
 
 class Place(db.Model):
-
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     movieid = db.Column(db.Integer, db.ForeignKey("movie.id"))
     name = db.Column(db.String(50))
     frequency = db.Column(db.Integer)
-    scene = db.Column(db.PickleType)
-    array = db.Column(db.JSON)
-
-
-class Summary(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(50))
 
 
 class Character(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    movieid = db.Column(db.Integer, db.ForeignKey("movie.id"))
     name = db.Column(db.String(50))
     lines = db.Column(db.Integer)
 
@@ -25,14 +18,32 @@ class Character(db.Model):
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(200))
+    author = db.Column(db.String(100))
+    totalCharacters = db.Column(db.Integer)
+    totalPages = db.Column(db.Integer)
+    totalPlaces = db.Column(db.Integer)
+    totalScenes = db.Column(db.Integer)
+
 
 class Scene(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     num = db.Column(db.Integer, unique=True)
     movieid = db.Column(db.Integer, db.ForeignKey("movie.id"))
+    placeid = db.Column(db.Integer, db.ForeignKey("place.id"))
+    timeid = db.Column(db.Integer, db.ForeignKey("time.id"))
 
-class Line(db.Model):
+
+class Time(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    scenenum = db.Column(db.Integer, db.ForeignKey("scene.num"))
-    content = db.Column(db.Text)
-    order = db.Column(db.SMALLINT)
+    name = db.Column(db.String(50))
+    movieid = db.Column(db.Integer, db.ForeignKey("movie.id"))
+    frequency = db.Column(db.Integer)
+
+
+class TimeCharacter(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    movieid = db.Column(db.Integer, db.ForeignKey("movie.id"))
+    timeid = db.Column(db.Integer, db.ForeignKey("time.id"))
+    characterid = db.Column(db.Integer, db.ForeignKey("character.id"))
+    frequency = db.Column(db.Integer)
+
