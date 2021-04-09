7188d39ebeac915dec00ee86e6e62bfd6a477fa4
suram <swamys@naver.com>
2021-04-09 15:06:03 +0900

    기능 : 캐릭터별 중요도(빈도수) api 구현

--- backend/api/api.py
+++ backend/api/api.py
@@ -2,6 +2,7 @@ from flask_restful import Api
 from flask import Blueprint
 from resource.place import PlaceFrequency, PlaceScene
 from resource.query_movie import QueryMovie
+from resource.character import CharacterFrequency
 
 toyScriptApi = Blueprint("api", __name__)
 api = Api(toyScriptApi)
@@ -9,5 +10,8 @@ api = Api(toyScriptApi)
 api.add_resource(PlaceFrequency, '/place/frequency')
 api.add_resource(PlaceScene, '/place/scene')
 api.add_resource(QueryMovie, '/movie')
+api.add_resource(CharacterFrequency, '/character/frequency')
+
+
 
 
---
+++ backend/resource/character.py
@@ -0,0 +1,20 @@
+from flask_restful import Resource
+from model.models import Character
+
+class CharacterFrequency(Resource):
+
+
+    def get(self):
+
+        characterFrequency = []
+
+        characters = Character.query.all()
+
+        for character in characters:
+
+            object = {}
+            object['characterName'] = character.name
+            object['frequency'] = character.lines
+            characterFrequency.append(object)
+
+        return sorted(characterFrequency, key=lambda x : x['frequency'], reverse=True)
