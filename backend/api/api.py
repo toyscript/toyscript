6457dbe78e23dc07842f6c4b4e41d5df0b42e931
suram <swamys@naver.com>
2021-04-10 16:06:11 +0900

    기능 : 영화 대본 개요 api 구현

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,7 +1,7 @@
 from flask_restful import Api
 from flask import Blueprint
 from resource.place import PlaceFrequency, PlaceScene, PlaceCharacter
-from resource.query_movie import QueryMovie
+from resource.movie import QueryMovie, Summary
 from resource.character import CharacterFrequency
 from resource.time import TimeScene, TimeCharacter
 
@@ -15,4 +15,5 @@ api.add_resource(QueryMovie, '/movie')
 api.add_resource(CharacterFrequency, '/character/frequency/<int:movieid>')
 api.add_resource(TimeScene, '/time/scene/<int:movieid>')
 api.add_resource(TimeCharacter, '/time/character/<int:movieid>')
+api.add_resource(Summary, '/movie/<int:movieid>')
 
