cb13ebb11f5387c5360c3d91beff5cf1a9326271
bky373 <bo373@naver.com>
2021-04-10 10:49:24 +0900

    기능 : 캐릭터가 자주 사용하는 단어 top 5 추출

--- backend/da/characterAnalysis.py
+++ backend/da/characterAnalysis.py
@@ -12,6 +12,8 @@ stopWords = set(stopwords.words("english"))
 pst = PorterStemmer()
 wnl = WordNetLemmatizer()
 
+
+# 캐릭터 대사 추출
 dialogues = " ".join(characterDialogues["WOODY"])
 
 # 대사 토큰화
@@ -19,7 +21,9 @@ tokens = word_tokenize(dialogues)
 
 # 불용어 제거 및 stemming
 # tokensWithoutStopWords = [pst.stem(token) for token in tokens if not token in stopWords]  # Stemming: 어간 추출
-tokensWithoutStopWords = [wnl.lemmatize(token) for token in tokens if not token in stopWords]  #  Lemmatization: 표제어 추출
+tokensWithoutStopWords = [
+    wnl.lemmatize(token) for token in tokens if not token in stopWords
+]  #  Lemmatization: 표제어 추출
 
 # 특수 문자 제거
 keyTokens = [
@@ -35,18 +39,23 @@ keyTokens = [
     for token in tokensWithoutStopWords
 ]
 
-# 토큰별 빈도 수 추출
-# print(Counter(keyTokens).most_common())
-
 # 빈 문자열 제거
-keyTokens = ' '.join([token for token in keyTokens if token])
+keyTokens = [token for token in keyTokens if token]
+
+
+# 토큰별 빈도 수 추출
+# print(Counter(keyTokens).most_common()[:7])
+
+
+keyTokens = " ".join(keyTokens)
 
 # 감정 분석
 # sia = SentimentIntensityAnalyzer()
-# scores = sia.polarity_scores(" ".join(keyTokens))
+# scores = sia.polarity_scores(keyTokens)
 # print(scores)
 
+
 # 워드 클라우드
-word_cloud = WordCloud(mask=np.array(Image.open('woody_black.png')))
-word_cloud.generate(keyTokens)
-word_cloud.to_file('wordcloud_woody.png')
+# word_cloud = WordCloud(mask=np.array(Image.open('woody_black.png')))
+# word_cloud.generate(keyTokens)
+# word_cloud.to_file('wordcloud_woody.png')
