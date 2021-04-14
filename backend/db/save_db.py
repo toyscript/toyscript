25584122b86367f1da1b554e27be3c1ad6450307
suram <swamys@naver.com>
2021-04-14 15:02:32 +0900

    리팩 : import 모듈 못 찾는에러 해결

--- backend/db/save_db.py
+++ backend/db/save_db.py
@@ -1,7 +1,6 @@
 from init_app.init_app import create_app
+import sys, os
+sys.path.append(os.path.abspath('../da'))
 
-app = create_app()
-
-from da.characterAnalysis import get_emotion_frequencies_by_character
-
-result = get_emotion_frequencies_by_character()
+from da.characters import character_dialogues
+result = character_dialogues
