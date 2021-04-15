f03f6ff97d2bcf53c6669aa36eadb13e1733794f
yuna kim <dbsk621@naver.com>
2021-04-15 14:27:30 +0900

    문서 : 버즈 이미지 임시 업로드

--- frontend/src/components/Contents.js
+++ frontend/src/components/Contents.js
@@ -11,31 +11,39 @@ function ControlledTabs() {
 
 
   return (
-    <Container>
-      <Tabs
-        className="Tabs"
-        activeKey={key}
-        onSelect={(k) => setKey(k)}
-      >
-        <Tab id="summary" eventKey="summary" title="Summary">
-          <Summary />
-        </Tab>
-        <Tab id="character" eventKey="character" title="Character">
-          <Container>
-            <p style={{backgroundColor:"rgb(246, 238, 204)"}}>
-              character page
-            </p>
-          </Container>
-        </Tab>
-        <Tab id="place" eventKey="place" title="Place">
-          <Place />
-        </Tab>
-        <Tab id="time" eventKey="time" title="Time">
-          <Time />
-        </Tab>
-      </Tabs>
-    </Container>
-    
+    <>
+      <Container className="Contents">
+        <Tabs
+          className="Tabs"
+          activeKey={key}
+          onSelect={(k) => setKey(k)}
+        >
+          <Tab id="summary" eventKey="summary" title="Summary">
+            <Summary />
+          </Tab>
+          <Tab id="character" eventKey="character" title="Character">
+            <Container>
+              <p style={{backgroundColor:"rgb(246, 238, 204)"}}>
+                character page
+              </p>
+            </Container>
+          </Tab>
+          <Tab id="place" eventKey="place" title="Place">
+            <Place />
+          </Tab>
+          <Tab id="time" eventKey="time" title="Time">
+            <Time />
+          </Tab>
+        </Tabs>
+      </Container>
+
+      <img
+        src="/images/buzz.png"
+        className="buzz"
+        alt="helper buzz"
+      />
+
+    </>
   );
 }
 
