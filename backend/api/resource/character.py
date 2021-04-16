63c581973dc5c3c78cc372d9e298e487a134566f
suram <swamys@naver.com>
2021-04-16 14:36:58 +0900

    픽스 : 워드클라우드 변수명 수정

--- backend/api/resource/character.py
+++ backend/api/resource/character.py
@@ -89,7 +89,7 @@ class CharacterWord(Resource):
 
             else:
                 tmp.get(wc.character.id).append(
-                    {"word": wc.word, "frequency": wc.frequency}
+                    {"text": wc.word, "value": wc.frequency}
                 )
 
         return result
