c18d2bb1cc3f648c5ba96265b41cf52e78455932
yuna kim <dbsk621@naver.com>
2021-05-06 16:19:54 +0900

    리팩 : 라이브러리 변경 및 css 파일삭제

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,30 +1,40 @@
 import React from "react";
-import { Nav, Navbar } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "../css/header.css"
+import { AppBar, Grid, Toolbar} from "@material-ui/core";
+import { Link } from "react-router-dom";
+
 
 function Header() {
-
   return(
     <>
-      <Navbar className="menuBar" expand="lg" sticky="top">
-        <Nav.Link href="/">
-          <img
-            src="/images/home.png"
-            className="home"
-            alt="home"
-          />
-        </Nav.Link>
-        <Nav.Link className="ml-auto" href="/ServiceInfo">
-          <img
-            src="/images/menu1.png"
-            className="info"
-            alt="Service information"
-          />
-        </Nav.Link>
-      </Navbar>
+      <AppBar
+        position="static"
+        style={{ backgroundColor : "rgb(246,233,180" }}
+      >
+        <Toolbar>
+          <Grid justify="space-between" container>
+            <Grid item>
+              <Link to="/">
+                <img 
+                  src="/images/home.png"
+                  style = {{ width: "110px", height:"40px" }}
+                  alt="home"
+                />
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link to="/ServiceInfo">
+                <img 
+                  src="/images/menu1.png"
+                  style = {{ width: "200px", height:"25px", alignItems:"flex-end" }}
+                  alt="service information page"
+                />
+              </Link>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
     </>
-  )
+  );
 }
 
 
